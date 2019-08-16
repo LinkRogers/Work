45658bf03078d402dc875af1dba9961b43f1d8b7
test <test@example.com>
2019-08-16 03:29:29 -0700

    gaieauto1 init

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --runtime=nvidia -p 8888:8888 -v $PWD:/tmp -w /tmp --rm -it jupyter:gpu bash
+docker run -p 8888:8888 -v $PWD:/tmp -w /tmp --rm -it linkrogers/research:tensorflow-jupyter-gpu bash
